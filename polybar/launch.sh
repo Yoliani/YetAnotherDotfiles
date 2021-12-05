c8dc7f0559a194b16cbbc5a13e1d8047850b0e4f
Yoliani <edrayoca@outlook.es>
2021-12-04 21:57:59 -0500

    commit

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
+#killall -q polybar
 
-# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# polybar -c ~/.config/polybar/config -rq bar &
+polybar -c ~/.config/polybar/config.ini -rq bar &
 
-# Launch Polybar
-polybar top -c ~/.config/polybar/config.ini &
+echo "Bars launched..."
